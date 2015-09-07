5e480f42f5e1a019b9e27647b868f7439e1d6eab
Nishutosh Sharma <nishutosh.sharma@trantorinc.com>
2015-09-07 17:00:36 +0530

    test

---
+++ spec/features/drag_drop_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.feature "Sortable", type: :feature do
+  describe "drag_drop" do
+    context "slide desktop page" do
+      it "should drag slide from library to carosule", :run_on_sauce => true do
+        visit "http://google.co.in"
+        # fill_in "#username", :with => "myusername"
+        # fill_in "#lst-ib", :with => "Completed the test"
+        click_button "Google Search"
+        expect "2"=="2"
+      end
+    end
+  end
+end
